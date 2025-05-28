197a9c4dd36992ef52e742746424b9fee7fbf692
minitpatil <minit.n.patil@gmail.com>
2025-05-28 10:58:58 +0530

    new component add

---
+++ Redux/src/component/2ndcomp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SecondComp() {
+
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default SecondComp;
---
+++ Redux/src/component/displaycount.jsx
@@ -0,0 +1,8 @@
+// DisplayCount.js
+import React from 'react';
+
+function DisplayCount({ count }) {
+  return <h2>Shared Count: {count}</h2>;
+}
+
+export default DisplayCount;
